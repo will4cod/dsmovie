dfb24c4c9c36e66bf040b370d98dc38d9f9f6f94
William Fernandes <william4fernandes@gmail.com>
2022-03-13 15:12:38 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -8,12 +8,14 @@ type Props = {
 
 function MovieCard({ movie }: Props) {
 
+    console.log(movie.score);
+
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore score={movie.score} count={movie.count}  />
 
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>    
